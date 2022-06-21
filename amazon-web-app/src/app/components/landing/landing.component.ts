d928c151686fc6ecae51a96d02b4b34678f4b00d
Param Kamdar <paramkamdar623@gmail.com>
2022-06-22 00:25:16 +0530

    Header, footer, landing page

--- amazon-web-app/src/app/components/landing/landing.component.ts
+++ amazon-web-app/src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  images: any = ["landing_1", "landing_2", "landing_3", "landing_4", "landing_5", "landing_6", "landing_7", "landing_8"];
+  products: any = [];
+  constructor(
+    private _productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this._productService.getProducts().subscribe(res => {
+      this.products = res.products;
+      console.log(this.products);
+    }, err => {
+      console.log("Something went wrong.")
+    });
   }
 
 }
--- amazon-web-app/src/app/services/product.service.ts
+++ amazon-web-app/src/app/services/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  pathAPI = "https://dummyjson.com/products";
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get<any>(this.pathAPI);
+  }
 }
